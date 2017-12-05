86b29380e4d50b1330666450390315e614a9901d
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2017-12-05 13:23:53 +0000

    Day five

---
+++ day_5/challenge_2.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+with open('/where_i_write_hacky_code/advent/day_5/input_file.txt') as f:
+    data = f.read()
+
+data = data.rstrip().split('\n')
+data = [int(line) for line in data]
+
+jumps = 0
+current_position = 0
+
+while True:
+    try:
+        destination = data[current_position] + current_position
+
+        if data[current_position] >= 3:
+            data[current_position] -= 1
+        else:
+            data[current_position] += 1
+
+        jumps += 1
+
+        current_position = destination
+
+    except(IndexError):
+        break
+
+print(jumps)
