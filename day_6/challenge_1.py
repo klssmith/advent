b32c6aa2ebe0c41de1e76d4ccc36ac286a67af39
Katie Smith <klssmith@users.noreply.github.com>
2017-12-06 07:24:28 +0000

    Day six

---
+++ day_6/challenge_1.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+with open('/where_i_write_hacky_code/advent/day_6/input_file.txt') as f:
+    data = f.read()
+
+data = data.split()
+banks = [int(x) for x in data]
+
+cycles = []
+
+while not banks in cycles:
+    cycles.append(banks)
+    banks = banks[:]
+
+    highest_number = max(banks)
+    current_bank = banks.index(highest_number)
+    banks[current_bank] = 0
+
+    for i in range(highest_number):
+        if current_bank == len(banks) - 1:
+            current_bank = 0
+        else:
+            current_bank += 1
+
+        banks[current_bank] += 1
+
+print(len(cycles))
